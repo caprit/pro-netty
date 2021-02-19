9a87210341a1c6184c828e1e01cfdd95c7562ea9
caprit <jcaprit@126.com>
2021-02-19 16:36:36 +0800

    Initial commit

---
+++ src/main/resources/bin/stop.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+cd `dirname $0`
+BIN_DIR=`pwd`
+cd ..
+DEPLOY_DIR=`pwd`
+CONF_DIR=$DEPLOY_DIR/conf
+
+LOGS_DIR=$DEPLOY_DIR/logs
+if [ ! -d $LOGS_DIR ]; then
+    mkdir $LOGS_DIR
+fi
+
+log_file="$LOGS_DIR/log.stop"
+if [ -f $log_file ]; then
+    rm -rf $log_file
+fi
+exec 1>>$log_file
+
+SERVER_NAME=`sed '/dubbo.application.name/!d;s/.*=//' conf/server.properties | tr -d '\r'`
+
+if [ -z "$SERVER_NAME" ]; then
+    SERVER_NAME=`hostname`
+fi
+
+PIDS=`ps aux | grep java | grep "$CONF_DIR" |awk '{print $2}'`
+if [ -z "$PIDS" ]; then
+    echo "ERROR: The $SERVER_NAME does not started!"
+    exit 1
+fi
+
+if [ "$1" != "skip" ]; then
+    #$BIN_DIR/dump.sh
+    echo "skip dump..."
+fi
+
+echo -e "Stopping the $SERVER_NAME ...\c"
+for PID in $PIDS ; do
+    kill $PID > /dev/null 2>&1
+done
+
+echo "wait stop server..."
+
+COUNT=0
+while [ $COUNT -lt 1 ]; do    
+    echo -e ".\c"
+    sleep 1
+    COUNT=1
+    for PID in $PIDS ; do
+        PID_EXIST=`ps -f -p $PID | grep java`
+        if [ -n "$PID_EXIST" ]; then
+            COUNT=0
+            break
+        fi
+    done
+done
+
+echo "OK!"
+echo "PID: $PIDS"
